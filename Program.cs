748944e78248c6cfb93f2c436af8b3798ad395ee
Dingless <mohameddeg.abdulahi@elev.ga.ntig.se>
2024-02-05 12:29:47 +0100

    kommeterat

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ static  void Main(string[] args)
   Hello32();
 
 }
- public static void Hello32()
+ public static void Hello32() // här skapas en public metod som jag kallar hello32
 {
     for (int i = 0; i < 32; i++) // for loop för att skriva ut HelloWorld 32 gånger 
     {
