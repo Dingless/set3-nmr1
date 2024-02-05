b16953033a967684259146b98d330f1fbe3eff8f
Dingless <mohameddeg.abdulahi@elev.ga.ntig.se>
2024-02-05 12:22:33 +0100

    set 3 uppg 1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using System;
+
+public class HelloWorld // detta är klassen Helloworld
+{
+
+public void Hello32() // skapar en public metod som ja namnger Hello32
+{
+    for (int i = 0; i < 32; i++) // for loop för att skriva ut HelloWorld 32 gånger 
+    {
+      Console.WriteLine("Hello, World");  
+    }
+
+
+}
+
+
+}
