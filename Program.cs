4784d8b7156bb004a85a481971881f6ad5471af6
Dingless <mohameddeg.abdulahi@elev.ga.ntig.se>
2024-02-05 12:28:47 +0100

    hade problem la inte in en Main metod alltsp en ingångspunkt

--- Program.cs
+++ Program.cs
@@ -3,7 +3,12 @@
 public class HelloWorld // detta är klassen Helloworld
 {
 
-public void Hello32() // skapar en public metod som ja namnger Hello32
+static  void Main(string[] args) 
+{
+  Hello32();
+
+}
+ public static void Hello32()
 {
     for (int i = 0; i < 32; i++) // for loop för att skriva ut HelloWorld 32 gånger 
     {
